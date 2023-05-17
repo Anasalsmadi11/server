cbe6ea88deed48cf6e08345d31001be59e129941
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2023-05-17 08:21:03 +0000

    add memes

--- schema.sql
+++ schema.sql
@@ -1,10 +1,19 @@
-DROP TABLE IF EXISTS favrecipe;
-CREATE TABLE IF NOT EXISTS favrecipe(
-id SERIAL PRIMARY KEY ,
-title varchar(255),
-readyinminutes varchar(255),
-image varchar(255),
-summary varchar(255)
+-- DROP TABLE IF EXISTS favrecipe;
+-- CREATE TABLE IF NOT EXISTS favrecipe(
+-- id SERIAL PRIMARY KEY ,
+-- title varchar(255),
+-- readyinminutes varchar(255),
+-- image varchar(255),
+-- summary varchar(255)
+-- );
+
+
+DROP TABLE IF EXISTS memes;
+CREATE TABLE IF NOT EXISTS memes (
+    id SERIAL PRIMARY KEY,
+    image_path VARCHAR(255),
+    meme_name VARCHAR(255),
+    rank INTEGER,
+    tags VARCHAR(255),
+    top_text VARCHAR(255)
 );
-
-
--- seed.sql
+++ seed.sql
@@ -1,8 +1,11 @@
-INSERT into favrecipe (title,readyinminutes,image,summary) values ('Spaghetti Bolognese', '30 minutes', 'https://www.example.com/spaghetti-bolognese.jpg', 'A classic Italian pasta dish with a rich meaty sauce.');
+-- INSERT into favrecipe (title,readyinminutes,image,summary) values ('Spaghetti Bolognese', '30 minutes', 'https://www.example.com/spaghetti-bolognese.jpg', 'A classic Italian pasta dish with a rich meaty sauce.');
 
-INSERT into favrecipe (title,readyinminutes,image,summary) values ('Chicken Tikka Masala', '45 minutes', 'https://www.example.com/chicken-tikka-masala.jpg', 'A spicy and creamy Indian curry dish with marinated chicken.');
+-- INSERT into favrecipe (title,readyinminutes,image,summary) values ('Chicken Tikka Masala', '45 minutes', 'https://www.example.com/chicken-tikka-masala.jpg', 'A spicy and creamy Indian curry dish with marinated chicken.');
 
-INSERT into favrecipe (title,readyinminutes,image,summary) values ('Mushroom Risotto', '1 hour', 'https://www.example.com/mushroom-risotto.jpg', 'A comforting and hearty Italian rice dish with sautéed mushrooms.');
+-- INSERT into favrecipe (title,readyinminutes,image,summary) values ('Mushroom Risotto', '1 hour', 'https://www.example.com/mushroom-risotto.jpg', 'A comforting and hearty Italian rice dish with sautéed mushrooms.');
 
-INSERT into favrecipe (title,readyinminutes,image,summary) values ('Fish and Chips', '40 minutes', 'https://www.example.com/fish-and-chips.jpg', 'A classic British pub dish with crispy battered fish and chunky fries.');
+-- INSERT into favrecipe (title,readyinminutes,image,summary) values ('Fish and Chips', '40 minutes', 'https://www.example.com/fish-and-chips.jpg', 'A classic British pub dish with crispy battered fish and chunky fries.');
 
+INSERT into memes (image_path,meme_name,rank,tags,top_text) values ("https://imgflip.com/s/meme/First-World-Problems.jpg", "First World Problems", 60,"Crying, FWP, White Whine", null);
+
+INSERT into memes (image_path,meme_name,rank,tags,top_text) values ("https://imgflip.com/s/meme/Batman-Slapping-Robin.jpg", "Batman Slapping Robin", 10,"My parents are dead", "");
